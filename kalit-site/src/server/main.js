abef1749bd0a656699b3c3fb64be08e7e92c4388
Nahom Tsegaye <tnahom214@gmail.com>
2024-11-03 16:30:19 +0300

    try-4

--- kalit-site/src/server/main.js
+++ kalit-site/src/server/main.js
@@ -399,7 +399,7 @@ ViteExpress.listen(app, 3000, () =>
 // Set the webhook for Telegram
 const setWebhook = async () => {
     try {
-        const webhookUrl = `https://kalit-watch-store-5lk7-15ylczgnm-nahomt23s-projects.vercel.app/${TELEGRAM_BOT_TOKEN}`;
+        const webhookUrl = `https://kalit-watch-store-5lk7-git-main-nahomt23s-projects.vercel.app/${TELEGRAM_BOT_TOKEN}`;
         const response = await axios.post(
             `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/setWebhook`,
             {
