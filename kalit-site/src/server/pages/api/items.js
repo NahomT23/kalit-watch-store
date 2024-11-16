d789c4774947eaa1c2411c1de89ebceacaaeba4f
Nahom Tsegaye <tnahom214@gmail.com>
2024-11-16 21:05:49 +0300

    try-9

---
+++ kalit-site/src/server/pages/api/items.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+const TELEGRAM_BOT_TOKEN = '7278024058:AAF4TbYuLfyJMjXMMBdirLx0aYFgffiDHcg'
+
+let items = [];
+
+const getFileUrl = async (fileId) => {
+    try {
+        const filePathResponse = await axios.get(
+            `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/getFile?file_id=${fileId}`
+        );
+        const filePath = filePathResponse.data.result.file_path;
+        return `https://api.telegram.org/file/bot${TELEGRAM_BOT_TOKEN}/${filePath}`;
+    } catch (error) {
+        console.error("Error fetching image:", error);
+        return null;
+    }
+};
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        res.json(items);
+    } else {
+        res.status(405).send('Method Not Allowed');
+    }
+}
---
+++ kalit-site/src/server/pages/api/webhook.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const TELEGRAM_BOT_TOKEN = '7278024058:AAF4TbYuLfyJMjXMMBdirLx0aYFgffiDHcg'
+
+
+
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        try {
+            const webhookUrl = `https://7c1c-102-213-69-44.ngrok-free.app/${TELEGRAM_BOT_TOKEN}`;
+            const response = await axios.post(
+                `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/setWebhook`,
+                { url: webhookUrl }
+            );
+
+            console.log("Webhook set:", response.data);
+            res.status(200).send("Webhook set successfully");
+        } catch (error) {
+            console.error("Error setting webhook:", error);
+            res.status(500).send("Error setting webhook");
+        }
+    } else {
+        res.status(405).send('Method Not Allowed');
+    }
+}
