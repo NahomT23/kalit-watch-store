5e13aae6603ef738cc97b6ee1bd75c645fae6e1d
Nahom Tsegaye <tnahom214@gmail.com>
2024-11-16 21:32:01 +0300

    try-9.5

--- kalit-site/src/server/api/sendOrderToTelegram.js
+++ kalit-site/src/server/api/sendOrderToTelegram.js
@@ -1,53 +1,53 @@
-import axios from "axios";
-import { configDotenv } from "dotenv";
+// import axios from "axios";
+// import { configDotenv } from "dotenv";
 
-configDotenv();
+// configDotenv();
 
-const TELEGRAM_BOT_TOKEN = '7278024058:AAF4TbYuLfyJMjXMMBdirLx0aYFgffiDHcg'
-const TELEGRAM_CHAT_ID = '696144741'
+// const TELEGRAM_BOT_TOKEN = '7278024058:AAF4TbYuLfyJMjXMMBdirLx0aYFgffiDHcg'
+// const TELEGRAM_CHAT_ID = '696144741'
 
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { totalAmount, items, userId, userName } = req.body;
-        const dateTime = new Date().toLocaleString();
+// export default async function handler(req, res) {
+//     if (req.method === 'POST') {
+//         const { totalAmount, items, userId, userName } = req.body;
+//         const dateTime = new Date().toLocaleString();
 
-        try {
-            const itemsList = items.map(item => `
-                Name: ${item.name}
-                Description: ${item.description}
-                Quantity: ${item.quantity}
-                Price: ${item.price}
-            `).join("\n\n");
+//         try {
+//             const itemsList = items.map(item => `
+//                 Name: ${item.name}
+//                 Description: ${item.description}
+//                 Quantity: ${item.quantity}
+//                 Price: ${item.price}
+//             `).join("\n\n");
 
-            const message = `
-                **New Order Received**
+//             const message = `
+//                 **New Order Received**
 
-                - User ID: ${userId}
-                - User Email: ${userName}
+//                 - User ID: ${userId}
+//                 - User Email: ${userName}
 
-                **Total Amount: ${totalAmount}**
+//                 **Total Amount: ${totalAmount}**
 
-                **Items:**
-                ${itemsList}
+//                 **Items:**
+//                 ${itemsList}
 
-                **Order Date: ${dateTime}**
-            `;
+//                 **Order Date: ${dateTime}**
+//             `;
 
-            await axios.post(
-                `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
-                {
-                    chat_id: TELEGRAM_CHAT_ID,
-                    text: message,
-                    parse_mode: 'Markdown',
-                }
-            );
+//             await axios.post(
+//                 `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
+//                 {
+//                     chat_id: TELEGRAM_CHAT_ID,
+//                     text: message,
+//                     parse_mode: 'Markdown',
+//                 }
+//             );
 
-            res.status(200).send("Order sent to Telegram");
-        } catch (error) {
-            console.error("Error sending order to Telegram:", error);
-            res.status(500).send("Error sending order to Telegram");
-        }
-    } else {
-        res.status(405).send('Method Not Allowed');
-    }
-}
+//             res.status(200).send("Order sent to Telegram");
+//         } catch (error) {
+//             console.error("Error sending order to Telegram:", error);
+//             res.status(500).send("Error sending order to Telegram");
+//         }
+//     } else {
+//         res.status(405).send('Method Not Allowed');
+//     }
+// }
